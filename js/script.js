d1fec973b63abc2cad2ef486ec12a42ade8b9fa6
roller40083 <83974394+roller40083@users.noreply.github.com>
2025-10-29 21:36:23 +0300

    удалил увелечение фото по минатюре

--- js/script.js
+++ js/script.js
@@ -112,6 +112,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // открыть по клику на миниатюре (один клик)
     
+    if (lightboxClose) lightboxClose.addEventListener('click', closeLightbox);
+    if (lightboxNext) lightboxNext.addEventListener('click', showNext);
+    if (lightboxPrev) lightboxPrev.addEventListener('click', showPrev);
 
     document.addEventListener('keydown', (e) => {
         if (!lightbox || lightbox.getAttribute('aria-hidden') === 'true') return;
