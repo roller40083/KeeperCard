6807506d55c431c7e79cf88ae335287b50b51bde
roller40083 <83974394+roller40083@users.noreply.github.com>
2025-10-30 21:27:44 +0300

    сделал логотип и анмацию

--- js/script.js
+++ js/script.js
@@ -110,8 +110,8 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // открыть по клику на миниатюре (один клик)
-    
+
+
     if (lightboxClose) lightboxClose.addEventListener('click', closeLightbox);
     if (lightboxNext) lightboxNext.addEventListener('click', showNext);
     if (lightboxPrev) lightboxPrev.addEventListener('click', showPrev);
